bfd54adfda56cf867eb5f4afae30bd23746ea48a
blewis-1 <lewisisraelb@gmail.com>
2024-06-09 19:12:26 -0400

    fix the loading issue with the app

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,43 +1,53 @@
-import { Provider as PaperProvider } from 'react-native-paper';
-import { lightTheme, darkTheme } from './Theme';
+import { NavigationContainer } from "@react-navigation/native";
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
+import { useCallback, useEffect } from 'react';
+import { View } from 'react-native';
 
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import React, { useState } from 'react';
-import IntroScreen from './components/IntroScreen';
+// create a stack navigator
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Levels from './components/Levels';
 import HomeScreen from './components/HomeScreen';
-import Dashboard from './components/Dashboard';
 import Game from './components/Game';
-// import GlobalLeaderboard from './components/GlobalLeaderboard';
-// import UserHistory from './components/UserHistory';
+import Score from './components/Score';
 
-const Stack = createStackNavigator();
 
-const AppNavigator = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+SplashScreen.preventAutoHideAsync();
 
-  const toggleTheme = () => {
-    setIsDarkMode(!isDarkMode);
-  };
+const Stack = createNativeStackNavigator();
 
-  const selectedTheme = isDarkMode ? darkTheme : lightTheme;
+export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'Nunito-Black': require('./assets/fonts/Nunito-Black.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  useEffect(() => {
+    onLayoutRootView();
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
 
   return (
-    <PaperProvider theme={selectedTheme}>
-      <NavigationContainer theme={selectedTheme}>
-        <Stack.Navigator initialRouteName="IntroScreen" screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="IntroScreen" component={IntroScreen} />
-          <Stack.Screen name="HomeScreen">
-            {props => <HomeScreen {...props} toggleTheme={toggleTheme} isDarkMode={isDarkMode} />}
-          </Stack.Screen>
-          <Stack.Screen name="Dashboard" component={Dashboard} />
-          <Stack.Screen name="Game" component={Game} />
-          {/* <Stack.Screen name="GlobalLeaderboard" component={GlobalLeaderboard} />
-          <Stack.Screen name="UserHistory" component={UserHistory} /> */}
+    <NavigationContainer>
+      <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen
+            name="Home Screen"
+            component={HomeScreen}
+          />
+          <Stack.Screen name="Levels" component={Levels} options={{ title: 'Levels' }} />
+          <Stack.Screen name="Game" component={Game} options={{ title: 'Game' }} />
+          <Stack.Screen name="Score" component={Score} options={{ title: 'Score' }} />
         </Stack.Navigator>
-      </NavigationContainer>
-    </PaperProvider>
+      </View>
+    </NavigationContainer>
   );
-};
-
-export default AppNavigator;
+}
